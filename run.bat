4d356b5f4128b23e83e86c5f04a52b2af9eef4c7
entr0pia <30486766+entr0pia@users.noreply.github.com>
2021-07-24 12:49:35 +0800

    support for emmc devices

--- run.bat
+++ run.bat
@@ -2,15 +2,13 @@
 ::作者: entr0pia (风沐白)
 ::文件: run.sh
 ::描述: 移除Android固件(如Sony Xpeira 5II)的boot.img中的system_ext分区, 修复patch magisk后的bootloop问题
-::版本: v1.0
+::版本: v1.0.1
 
 set img=%1
 if "%img:~-3%" == "img" goto swicth
-
 if not "%img:~-3%" == "sin" goto end
 .\unsin.exe %img%
 set img=%img:sin=img%
-
 if not exist %img% goto end
 
 :swicth
@@ -18,25 +16,24 @@ git submodule foreach "git clean -xdf"
 git submodule set-branch -b iWin Android-Image-Kitchen
 git submodule update --remote -f
 
-move %img% Android-Image-Kitchen
+move %img% Android-Image-Kitchen\boot.img
 cd Android-Image-Kitchen
 call unpackimg.bat
 
 :sed
 setlocal enabledelayedexpansion
-set fstab=.\ramdisk\fstab.qcom
-set fstab_tmp=.\fstab.tmp
-
-for /f "delims=" %%i in (%fstab%) do (
+set fstab_tmp=\fstab.tmp
+for /R "." %%f in (fstab.*) do (
+for /f "delims=" %%i in (%%f) do (
     set str=%%i
     set "str=!str:system_ext=#system_ext!"
     echo !str!>>%fstab_tmp%
 )
-move %fstab_tmp% %fstab%
+move %fstab_tmp% %%f
+)
 
 call repackimg.bat
 move .\image-new.img ..
 git clean -xdf
 cd ..
-
 :end
--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 #作者: entr0pia (风沐白)
 #文件: run.sh
 #描述: 移除Android固件(如Sony Xpeira 5II)的boot.img中的system_ext分区, 修复patch magisk后的bootloop问题
-#版本: v1.0
+#版本: v1.0.1
 
 img=$1
-exname=${img##*\.}
-if [ "$exname" = "sin" ]; then
+
+if [ "${img##*\.}" = "sin" ]; then
     result=$(./unsin $1 | grep img)
     if [ -z "$result" ]; then
         exit
@@ -14,14 +14,22 @@ if [ "$exname" = "sin" ]; then
     img=${result##*/}
 fi
 
+if [ "${img##*\.}" != "img" ]; then
+    exit
+fi
+
+echo $img
+
 git submodule foreach 'git clean -xdf'
 git submodule set-branch -b iNux Android-Image-Kitchen
 git submodule update --remote -f
 
-mv $img Android-Image-Kitchen
+mv $img Android-Image-Kitchen/boot.img
 cd Android-Image-Kitchen
+
 ./unpackimg.sh
-sed -i 's/^system_ext/#system_ext/g' ramdisk/fstab.qcom
+find . -name "fstab.*" | xargs sed -i 's/^system_ext/#system_ext/g'
 ./repackimg.sh
+
 mv image-new.img ..
 git clean -xdf
